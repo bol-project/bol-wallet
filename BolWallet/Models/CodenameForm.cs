20476d07d1c6d1c37e80656a1a357a228f3984f1
Nikos Chomenidis <nchomenidis@indeavor.com>
2022-10-02 20:32:05 +0300

    Add attributes at CodenameForm in order to improve the UI

--- BolWallet/Models/CodenameForm.cs
+++ BolWallet/Models/CodenameForm.cs
@@ -5,21 +5,31 @@ namespace BolWallet.Models;
 
 public class CodenameForm
 {
+	[DataFormDisplayOptions(LabelIcon = "user", LabelWidth = "auto")]
 	[DataFormItemPosition(RowOrder = 1, ItemOrderInRow = 1)]
+	[DataFormTextEditor(InplaceLabelText = "Surname")]
 	public string Surname { get; set; }
 
-	[DataFormItemPosition(RowOrder = 2, ItemOrderInRow = 1)]
+	[DataFormDisplayOptions(IsLabelVisible = false)]
+	[DataFormItemPosition(RowOrder = 1, ItemOrderInRow = 2)]
+	[DataFormTextEditor(InplaceLabelText = "Middle name")]
 	public string MiddleName { get; set; }
 
-	[DataFormItemPosition(RowOrder = 2, ItemOrderInRow = 2)]
+	[DataFormDisplayOptions(IsLabelVisible = false)]
+	[DataFormTextEditor(InplaceLabelText = "Third name")]
+	[DataFormItemPosition(RowOrder = 2, ItemOrderInRow = 1)]
 	public string ThirdName { get; set; }
 
-	public DateTime DateOfBirth { get; set; }
-	
+	[DataFormDisplayOptions(LabelWidth = "auto")]
+	public DateTime? Birthday { get; set; }
+
+	[DataFormDisplayOptions(LabelWidth = "auto")]
 	public Gender Gender { get; set; }
 
+	[DataFormDisplayOptions(LabelWidth = "auto")]
 	[DataFormComboBoxEditor]
 	public string Country { get; set; }
 
+	[DataFormDisplayOptions(LabelWidth = "auto")]
 	public string Combination { get; set; }
 }
