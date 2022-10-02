ad601548897f7493a3b1ebc54e730772c3fc3b6d
Orestis Giannoukos <orestisg@me.com>
2022-10-02 23:10:18 +0300

    Rename file with GlobalUsings

--- BolWallet/Usings.cs
+++
@@ -1,7 +0,0 @@
-global using System.Threading.Tasks;
-global using System.Collections;
-global using BolWallet.Models;
-global using BolWallet.Services;
-global using BolWallet.ViewModels;
-global using BolWallet.Views;
-global using Microsoft.Maui.Controls;
