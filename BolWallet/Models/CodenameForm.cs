f07d83a57286f7662244dee07cedb13bbd2269b1
Nikos Chomenidis <nchomenidis@indeavor.com>
2022-10-02 04:42:30 +0300

    Add form item position attributes and re-order properties

--- BolWallet/Models/CodenameForm.cs
+++ BolWallet/Models/CodenameForm.cs
@@ -5,18 +5,21 @@ namespace BolWallet.Models;
 
 public class CodenameForm
 {
-	[DataFormComboBoxEditor]
-	public string Country { get; set; }
-
+	[DataFormItemPosition(RowOrder = 1, ItemOrderInRow = 1)]
 	public string Surname { get; set; }
 
+	[DataFormItemPosition(RowOrder = 2, ItemOrderInRow = 1)]
 	public string MiddleName { get; set; }
 
+	[DataFormItemPosition(RowOrder = 2, ItemOrderInRow = 2)]
 	public string ThirdName { get; set; }
 
+	public DateTime DateOfBirth { get; set; }
+	
 	public Gender Gender { get; set; }
 
-	public DateTime DateOfBirth { get; set; }
+	[DataFormComboBoxEditor]
+	public string Country { get; set; }
 
 	public string Combination { get; set; }
 }
