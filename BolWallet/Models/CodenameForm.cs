63e013773f4cdf29beb9dea5215747ed714daa37
Orestis Giannoukos <orestisg@me.com>
2022-10-03 22:29:56 +0300

    Enhance Codename Form

--- BolWallet/Models/CodenameForm.cs
+++ BolWallet/Models/CodenameForm.cs
@@ -6,34 +6,46 @@ namespace BolWallet.Models;
 
 public class CodenameForm
 {
-	[DataFormDisplayOptions(LabelText = "\ue7fd", LabelWidth = "auto")]
+	[DataFormDisplayOptions(LabelText = "\ue7fd")]
 	[DataFormItemPosition(RowOrder = 1, ItemOrderInRow = 1)]
+	[DataFormTextEditor(InplaceLabelText = "Firstname")]
+	[RegularExpression("[A-Z]+", ErrorMessage = "Firstname should be only in capital letters")]
+	public string Firstname { get; set; }
+
+	[DataFormDisplayOptions(LabelText = "", LabelWidth = "auto")]
+	[DataFormItemPosition(RowOrder = 1, ItemOrderInRow = 2)]
 	[DataFormTextEditor(InplaceLabelText = "Surname")]
+	[RegularExpression("[A-Z]+", ErrorMessage = "Surname should be only in capital letters")]
 	public string Surname { get; set; }
 
-	[DataFormDisplayOptions(IsLabelVisible = false)]
-	[DataFormItemPosition(RowOrder = 1, ItemOrderInRow = 2)]
+	[DataFormDisplayOptions(LabelText = "")]
+	[DataFormItemPosition(RowOrder = 2, ItemOrderInRow = 1)]
 	[DataFormTextEditor(InplaceLabelText = "Middle name")]
+	[RegularExpression("[A-Z]*", ErrorMessage = "Middle name should be only in capital letters")]
 	public string MiddleName { get; set; }
 
-	[DataFormDisplayOptions(IsLabelVisible = false)]
+	[DataFormDisplayOptions(LabelText = "", LabelWidth = "auto")]
+	[DataFormItemPosition(RowOrder = 2, ItemOrderInRow = 2)]
 	[DataFormTextEditor(InplaceLabelText = "Third name")]
-	[DataFormItemPosition(RowOrder = 1, ItemOrderInRow = 3)]
+	[RegularExpression("[A-Z]*", ErrorMessage = "Third name should be only in capital letters")]
 	public string ThirdName { get; set; }
 
-	[DataFormDisplayOptions(LabelText = "\ue7e9", LabelWidth = "auto")]
-	[DataFormTextEditor(InplaceLabelText = "Birthday")]
+	[DataFormDisplayOptions(LabelText = "\ue7e9")]
+	[DataFormDateEditor(InplaceLabelText = "Birthday")]
 	public DateTime? Birthday { get; set; }
 
-	[DataFormDisplayOptions(LabelText = "\uf8d9", LabelWidth = "auto")]
+	[DataFormDisplayOptions(LabelText = "\uf8d9")]
 	[DataFormComboBoxEditor(InplaceLabelText = "Gender")]
 	public Gender Gender { get; set; }
 
-	[DataFormDisplayOptions(LabelText = "\ue80b", LabelWidth = "auto")]
-	[DataFormComboBoxEditor(InplaceLabelText = "Country")]
+	[DataFormDisplayOptions(LabelText = "\ue80b")]
+	[DataFormComboBoxEditor(
+		InplaceLabelText = "Country",
+		ValueMember = nameof(Models.Country.Alpha3),
+		DisplayMember = nameof(Models.Country.Name))]
 	public string Country { get; set; }
 
-	[DataFormDisplayOptions(LabelText = "\ue9f4", LabelWidth = "auto", HelpText = "To be added")]
+	[DataFormDisplayOptions(LabelText = "\ue9f4", HelpText = "To be added")]
 	[DataFormTextEditor(InplaceLabelText = "Combination")]
 	[MaxLength(1, ErrorMessage = "Combination should be only one digit/letter")]
 	[RegularExpression("[A-Z0-9]", ErrorMessage = "Combination should be a capital letter or a number")]
