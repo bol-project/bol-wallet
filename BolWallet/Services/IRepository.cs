6a1ec58b6f2642032107ee4a65bf4cd71083369e
Nikos Chomenidis <nchomenidis@indeavor.com>
2022-10-08 00:22:37 +0300

    Update IRepository interface and add summaries

--- BolWallet/Services/IRepository.cs
+++ BolWallet/Services/IRepository.cs
@@ -8,7 +8,28 @@ public interface IRepository
 	Task CreateAsync<TEntity>(string key, TEntity entity, CancellationToken token = default)
 		where TEntity : class;
 
+	/// <summary>
+	/// Gets the value of this <see cref="key"/> from the secure encrypted storage.
+	/// </summary>
+	/// <returns></returns>
 	Task<string> GetSecureAsync(string key);
+	
+	/// <summary>
+	/// Gets the deserialized value of this <see cref="key"/> from the secure encrypted storage.
+	/// </summary>
+	/// <returns></returns>
+	Task<TEntity> GetSecureAsync<TEntity>(string key) where TEntity : class;
 
+	/// <summary>
+	/// Saves the key-value pair to the secure encrypted storage.
+	/// </summary>
+	/// <returns></returns>
 	Task CreateSecureAsync(string key, string value);
+	
+	/// <summary>
+	/// Saves the key-value pair to the secure encrypted storage.
+	/// The <see cref="entity"/> is saved as a json string.
+	/// </summary>
+	/// <returns></returns>
+	Task CreateSecureAsync<TEntity>(string key, TEntity entity) where TEntity : class;
 }
