8e22897ad4764ff75ae7d3e95a26f51de4e6f859
Nikos Chomenidis <nchomenidis@indeavor.com>
2022-10-09 18:07:06 +0300

    Add field for NinSpecifications at RegisterContext

--- BolWallet/Models/RegisterContent.cs
+++ BolWallet/Models/RegisterContent.cs
@@ -1,8 +1,11 @@
+using Bol.Core.Model;
+
 namespace BolWallet.Models;
 
 public class RegisterContent
 {
 	public IEnumerable<Country> Countries { get; set; }
+	public IDictionary<string, NinSpecification> NinPerCountryCode { get; set; }
 	public string Codename { get; set; }
 	public IDictionary<string, string> Edi { get; set; }
 }
