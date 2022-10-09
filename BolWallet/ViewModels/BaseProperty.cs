e8866316a20bb776b55c140fb6ecfec7da9a95e5
Orestis Giannoukos <orestisg@me.com>
2022-10-10 01:44:35 +0300

    Create BaseProperty
    This class contains common logic that is required to enhance form's fields with help messages, error messages and validations

---
+++ BolWallet/ViewModels/BaseProperty.cs
@@ -0,0 +1,39 @@
+﻿namespace BolWallet.ViewModels;
+
+public partial class BaseProperty : ObservableObject
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(HasError))]
+    [NotifyPropertyChangedFor(nameof(IsNotReady))]
+    [NotifyPropertyChangedFor(nameof(IsReady))]
+    private string _value;
+
+    public bool HasError
+    {
+        get => !string.IsNullOrEmpty(Value) && !IsValid(Value);
+    }
+
+    public bool IsMandatory { private get; set; }
+
+    public bool IsNotReady
+    {
+        get => IsMandatory && string.IsNullOrEmpty(Value);
+    }
+
+    public bool IsReady
+    {
+        get => !string.IsNullOrEmpty(Value) && IsValid(Value);
+    }
+
+    [ObservableProperty]
+    private string _errorMessage;
+
+    [ObservableProperty]
+    private string _helpMessage;
+
+    [ObservableProperty]
+    private string _label;
+
+    public Func<string, bool> IsValid { private get; set; }
+}
+
