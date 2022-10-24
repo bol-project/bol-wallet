cb0d4b5a5de3c9fd903fbb977c672aefcbf22502
Orestis Giannoukos <orestisg@me.com>
2022-10-24 22:20:19 +0300

    Remove  user related data from Register Content

--- BolWallet/Models/RegisterContent.cs
+++ BolWallet/Models/RegisterContent.cs
@@ -6,6 +6,4 @@ public class RegisterContent
 {
 	public IEnumerable<Country> Countries { get; set; }
 	public IDictionary<string, NinSpecification> NinPerCountryCode { get; set; }
-	public string Codename { get; set; }
-	public IDictionary<string, string> Edi { get; set; }
 }
