2c058cb0e5b8d7372e7bae7e0c0b052209d9ee9f
nmoschos <nmoschos@metamind.gr>
2023-08-29 20:18:35 +0300

    Implement dynamic registration of BolConfig using values from appsettings

--- BolWallet/MauiProgram.cs
+++ BolWallet/MauiProgram.cs
@@ -18,47 +18,56 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
+
 		builder
 			.UseMauiApp<App>()
-            .UseMauiCommunityToolkit()
-            .ConfigureFonts(fonts =>
+			.UseMauiCommunityToolkit()
+			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialIconsRegular");
-            });
-        var services = builder.Services;
+				fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialIconsRegular");
+			});
 
-        // Register Services
-        services.AddScoped<IRepository>(_ => new Repository(BlobCache.UserAccount));
-        services.AddScoped<ISecureRepository>(_ => new SecureRepository(SecureStorage.Default));
-        services.AddSingleton<INavigationService, NavigationService>();
-        services.AddScoped<ICountriesService, CountriesService>();
-        services.AddSingleton<IPermissionService, PermissionService>();
+		builder.AddConfiguration("BolWallet.appsettings.json");
 
-        services.AddSingleton<IMediaPicker, Services.MediaPicker>();
-        services.RegisterViewAndViewModelSubsystem();
+		var services = builder.Services;
 
-        services.AddBolSdk();
+		// Register Services
+		services.AddScoped<IRepository>(_ => new Repository(BlobCache.UserAccount));
+		services.AddScoped<ISecureRepository>(_ => new SecureRepository(SecureStorage.Default));
+		services.AddSingleton<INavigationService, NavigationService>();
+		services.AddScoped<ICountriesService, CountriesService>();
+		services.AddSingleton<IPermissionService, PermissionService>();
 
-        using var sp = services.BuildServiceProvider();
+		services.AddSingleton<IMediaPicker, Services.MediaPicker>();
+		services.RegisterViewAndViewModelSubsystem();
 
-        var countries = sp.GetRequiredService<IOptions<List<Bol.Core.Model.Country>>>().Value;
-        var ninSpecifications = sp.GetRequiredService<IOptions<List<NinSpecification>>>().Value;
-        var content = new RegisterContent
-        {
-            Countries = countries.Select(c => new Country { Alpha3 = c.Alpha3, Name = c.Name, Region = c.Region }).ToList(),
-            NinPerCountryCode = ninSpecifications.ToDictionary(n => n.CountryCode, n => n)
-        };
+		// Register RpcEndpoint and Contract
+		var bolConfig = new BolConfig();
+		builder.Configuration.GetSection("BolSettings").Bind(bolConfig);
+		services.AddSingleton(typeof(IOptions<BolConfig>), Microsoft.Extensions.Options.Options.Create(bolConfig));
 
-        // This model will hold the data from the Register flow
-        services.AddSingleton(content);
+		services.AddBolSdk();
+
+		using var sp = services.BuildServiceProvider();
+
+		var countries = sp.GetRequiredService<IOptions<List<Bol.Core.Model.Country>>>().Value;
+		var ninSpecifications = sp.GetRequiredService<IOptions<List<NinSpecification>>>().Value;
+		var content = new RegisterContent
+		{
+			Countries = countries.Select(c => new Country { Alpha3 = c.Alpha3, Name = c.Name, Region = c.Region }).ToList(),
+			NinPerCountryCode = ninSpecifications.ToDictionary(n => n.CountryCode, n => n)
+		};
+
+		// This model will hold the data from the Register flow
+		services.AddSingleton(content);
 
 		ConfigureWalletServices(services, sp);
 
 		Registrations.Start(AppInfo.Current.Name); // TODO stop BlobCache after quit
 
-        return builder.Build();
+		return builder.Build();
 	}
 
 	private static void ConfigureWalletServices(IServiceCollection services, ServiceProvider sp)
@@ -78,22 +87,22 @@ public static class MauiProgram
 	}
 
 	private static MauiAppBuilder AddConfiguration(this MauiAppBuilder builder, string appSettingsPath)
-    {
-        var assembly = Assembly.GetExecutingAssembly();
-        using var stream = assembly.GetManifestResourceStream(appSettingsPath);
+	{
+		var assembly = Assembly.GetExecutingAssembly();
+		using var stream = assembly.GetManifestResourceStream(appSettingsPath);
 
-        var configRoot = new ConfigurationBuilder()
-            .AddJsonStream(stream)
-            .Build();
+		var configRoot = new ConfigurationBuilder()
+			.AddJsonStream(stream)
+			.Build();
 
-        builder.Configuration.AddConfiguration(configRoot);
+		builder.Configuration.AddConfiguration(configRoot);
 
-        var configuration = builder.Services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+		var configuration = builder.Services.BuildServiceProvider().GetRequiredService<IConfiguration>();
 
-        var bolConfig = configuration.GetRequiredSection("BolSettings").Get<BolConfig>();
+		var bolConfig = configuration.GetRequiredSection("BolSettings").Get<BolConfig>();
 
-        builder.Services.AddSingleton(bolConfig);
+		builder.Services.AddSingleton(bolConfig);
 
-        return builder;
-    }
+		return builder;
+	}
 }
