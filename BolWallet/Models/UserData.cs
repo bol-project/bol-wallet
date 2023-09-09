a01aaeab9b090e4cab99b9aea93f9b0a03df4b91
nmoschos <nmoschos@metamind.gr>
2023-09-09 11:17:39 +0300

    Implement user data registration flagging functionality

--- BolWallet/Models/UserData.cs
+++ BolWallet/Models/UserData.cs
@@ -9,4 +9,5 @@ public class UserData
     public EncryptedDigitalMatrix EncryptedDigitalMatrix { get; set; }
     public Bol.Core.Model.BolWallet BolWallet { get; set; }
     public string WalletPassword { get; set; }
+    public bool IsRegisteredAccount { get; set; }
 }
