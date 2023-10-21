a2341e2661bc390e60c0650fb5888d54bc0ec459
Querios <vantsios@hotmail.com>
2023-10-21 19:44:51 +0300

    Added BolTransactionEntryListItem

---
+++ BolWallet/Models/BolTransactionEntryListItem.cs
@@ -0,0 +1,36 @@
+﻿using Bol.Core.Model;
+using System.Text.RegularExpressions;
+
+namespace BolWallet.Models;
+
+public partial class BolTransactionEntryListItem : ObservableObject
+{
+    public BolTransactionEntry BolTransactionEntry { get; set; }
+
+    private string UserCodeName { get; set; }
+
+    private bool _isExpanded;
+    public bool IsExpanded
+    {
+        get => _isExpanded;
+        set
+        {
+            _isExpanded = value;
+            OnPropertyChanged();
+        }
+    }
+    private bool IsRecievingTransaction => UserCodeName == BolTransactionEntry.ReceiverCodeName;
+    public string BolAmount => (IsRecievingTransaction? "+" : "-") + BolTransactionEntry.Amount + " BOL";
+
+    public Color AmountTextColor => IsRecievingTransaction ? Colors.LawnGreen : Colors.Red;  
+
+    public BolTransactionEntryListItem(string userCodeName, BolTransactionEntry bolTransactionEntry)
+    {
+        UserCodeName = userCodeName;
+        BolTransactionEntry = bolTransactionEntry;
+        IsExpanded = false;
+    }
+
+
+}
+
