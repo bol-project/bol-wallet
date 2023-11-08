ad8c96748f3475e36f6fb02fc65c7d2c4d3d3e4c
nmoschos <nmoschos@metamind.gr>
2023-11-08 15:37:51 +0200

    Implement error handling in wallet methods

--- BolWallet/ViewModels/CreateCodenameViewModel.cs
+++ BolWallet/ViewModels/CreateCodenameViewModel.cs
@@ -1,91 +1,99 @@
-using System.Globalization;
 using Bol.Core.Abstractions;
 using Bol.Core.Model;
+using CommunityToolkit.Maui.Alerts;
+using System.Globalization;
 
 namespace BolWallet.ViewModels;
 
 public partial class CreateCodenameViewModel : BaseViewModel
 {
-    private readonly ICodeNameService _codeNameService;
-    private readonly ISecureRepository _secureRepository;
+	private readonly ICodeNameService _codeNameService;
+	private readonly ISecureRepository _secureRepository;
 
-    public CreateCodenameViewModel(
-        INavigationService navigationService,
-        ICodeNameService codeNameService,
-        RegisterContent content,
-        ISecureRepository secureRepository)
-        : base(navigationService)
-    {
-        _codeNameService = codeNameService;
-        _secureRepository = secureRepository;
-        CodenameForm = new CodenameForm(content);
-    }
+	public CreateCodenameViewModel(
+		INavigationService navigationService,
+		ICodeNameService codeNameService,
+		RegisterContent content,
+		ISecureRepository secureRepository)
+		: base(navigationService)
+	{
+		_codeNameService = codeNameService;
+		_secureRepository = secureRepository;
+		CodenameForm = new CodenameForm(content);
+	}
 
-    [ObservableProperty]
-    private CodenameForm _codenameForm;
+	[ObservableProperty]
+	private CodenameForm _codenameForm;
 
-    [ObservableProperty]
-    private string _codename = " ";
+	[ObservableProperty]
+	private string _codename = " ";
 
 
-    [RelayCommand]
-    private Task Submit()
-    {
-        return NavigationService.NavigateTo<CreateEdiViewModel>(true);
-    }
-    
-    [RelayCommand]
-    private async Task Generate()
-    {
-        if (!CodenameForm.IsFormFilled)
-        {
-            return;
-        }
+	[RelayCommand]
+	private Task Submit()
+	{
+		return NavigationService.NavigateTo<CreateEdiViewModel>(true);
+	}
 
-        var person = new NaturalPerson
-        {
-            FirstName = CodenameForm.FirstName.Value,
-            MiddleName = CodenameForm.MiddleName.Value,
-            Surname = CodenameForm.Surname.Value,
-            ThirdName = CodenameForm.ThirdName.Value,
-            Gender = CodenameForm.Gender,
-            Combination = CodenameForm.Combination.Value,
-            Nin = CodenameForm.NIN.Value,
-            Birthdate = DateTime.Parse(CodenameForm.Birthdate.Value),
-            CountryCode = CodenameForm.SelectedCountry.Alpha3
-        };
+	[RelayCommand]
+	private async Task Generate()
+	{
+		try
+		{
+			if (!CodenameForm.IsFormFilled)
+			{
+				return;
+			}
 
-        var result = _codeNameService.Generate(person);
+			var person = new NaturalPerson
+			{
+				FirstName = CodenameForm.FirstName.Value,
+				MiddleName = CodenameForm.MiddleName.Value,
+				Surname = CodenameForm.Surname.Value,
+				ThirdName = CodenameForm.ThirdName.Value,
+				Gender = CodenameForm.Gender,
+				Combination = CodenameForm.Combination.Value,
+				Nin = CodenameForm.NIN.Value,
+				Birthdate = DateTime.Parse(CodenameForm.Birthdate.Value),
+				CountryCode = CodenameForm.SelectedCountry.Alpha3
+			};
 
-        var userData = new UserData
-        {
-            Codename = result,
-            Person = person
-        };
+			var result = _codeNameService.Generate(person);
 
-        await _secureRepository.SetAsync("userdata", userData);
-        
-        Codename = result;
-        CodenameForm.IsInvalidated = false;
-    }
+			var userData = new UserData
+			{
+				Codename = result,
+				Person = person
+			};
 
-    public async Task Initialize()
-    {
-        var userData = await _secureRepository.GetAsync<UserData>("userdata");
-        if (userData is null) return;
+			await _secureRepository.SetAsync("userdata", userData);
 
-        CodenameForm.FirstName.Value = userData.Person.FirstName;
-        CodenameForm.MiddleName.Value = userData.Person.MiddleName;
-        CodenameForm.Surname.Value = userData.Person.Surname;
-        CodenameForm.ThirdName.Value = userData.Person.ThirdName;
-        CodenameForm.Gender = userData.Person.Gender;
-        CodenameForm.Combination.Value = userData.Person.Combination;
-        CodenameForm.SelectedCountry = CodenameForm
-                    .Countries
-                    .FirstOrDefault(c => c.Alpha3 == userData.Person.CountryCode);
-        CodenameForm.NIN.Value = userData.Person.Nin;
-        CodenameForm.Birthdate.Value = userData.Person.Birthdate.ToString(CultureInfo.InvariantCulture);
-        
-        Codename = userData.Codename;
-    }
+			Codename = result;
+			CodenameForm.IsInvalidated = false;
+		}
+		catch (Exception ex)
+		{
+			await Toast.Make(ex.Message).Show();
+		}
+	}
+
+	public async Task Initialize()
+	{
+		var userData = await _secureRepository.GetAsync<UserData>("userdata");
+		if (userData is null) return;
+
+		CodenameForm.FirstName.Value = userData.Person.FirstName;
+		CodenameForm.MiddleName.Value = userData.Person.MiddleName;
+		CodenameForm.Surname.Value = userData.Person.Surname;
+		CodenameForm.ThirdName.Value = userData.Person.ThirdName;
+		CodenameForm.Gender = userData.Person.Gender;
+		CodenameForm.Combination.Value = userData.Person.Combination;
+		CodenameForm.SelectedCountry = CodenameForm
+					.Countries
+					.FirstOrDefault(c => c.Alpha3 == userData.Person.CountryCode);
+		CodenameForm.NIN.Value = userData.Person.Nin;
+		CodenameForm.Birthdate.Value = userData.Person.Birthdate.ToString(CultureInfo.InvariantCulture);
+
+		Codename = userData.Codename;
+	}
 }
