84c26ea7301e3f21dc2066ffd78a4196a245abbc
nmoschos <nmoschos@metamind.gr>
2023-12-16 11:50:25 +0200

    Check if codename already exists

--- BolWallet/ViewModels/CreateCodenameViewModel.cs
+++ BolWallet/ViewModels/CreateCodenameViewModel.cs
@@ -1,6 +1,7 @@
 using Bol.Core.Abstractions;
 using Bol.Core.Model;
 using CommunityToolkit.Maui.Alerts;
+using Newtonsoft.Json.Linq;
 using System.Globalization;
 
 namespace BolWallet.ViewModels;
@@ -60,7 +61,13 @@ public partial class CreateCodenameViewModel : BaseViewModel
 
 			var result = _codeNameService.Generate(person);
 
-			var userData = new UserData
+			if (IsCodenameExists(result))
+			{
+                Toast.Make("The codename already exists. Please create a different codename.").Show().Wait();
+				return;
+            }
+
+            var userData = new UserData
 			{
 				Codename = result,
 				Person = person
@@ -96,4 +103,39 @@ public partial class CreateCodenameViewModel : BaseViewModel
 
 		Codename = userData.Codename;
 	}
+
+
+    private static bool IsCodenameExists(string codename)
+    {
+        var client = new HttpClient();
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "https://rpcnode.demo.bolchain.net:443");
+
+        var stringContent = new StringContent("{\r\n\"jsonrpc\":\"2.0\",\r\n\"id\":1,\r\n\"method\":\"getAccount\",\r\n\"params\":[\"" + codename + "\"]\r\n}\r\n", null, "application/json");
+        request.Content = stringContent;
+
+        try
+        {
+            using (var response = client.SendAsync(request).Result)
+            {
+                response.EnsureSuccessStatusCode();
+
+                var resultJson = response.Content.ReadAsStringAsync().Result;
+
+                var jsonResponse = JObject.Parse(resultJson);
+
+                if (jsonResponse["error"] == null)
+                    return true;
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Toast.Make(ex.Message).Show().Wait();
+
+            return false;
+        }
+
+        return false;
+    }
 }
