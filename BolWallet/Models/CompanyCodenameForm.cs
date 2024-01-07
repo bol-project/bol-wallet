2f03b8f735d15a55fe083fee4749f30414a4abcc
Querios <vantsios@hotmail.com>
2024-01-07 23:28:30 +0200

    Created Company Codename Form model

---
+++ BolWallet/Models/CompanyCodenameForm.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bol.Core.Model;
+
+namespace BolWallet.Models;
+
+public partial class CompanyCodenameForm : ObservableObject
+{
+    private readonly RegisterContent _content;
+
+    public CompanyCodenameForm(RegisterContent content)
+    {
+        _content = content;
+    }
+
+    [Required]
+    public Bol.Core.Model.Country Country { get; set; }
+
+    [Required]
+    public OrgType OrgType { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    [Required]
+    public string VatNumber { get; set; }
+
+    [Required]
+    public DateTime IncorporationDate { get; set; }
+
+    [Required]
+    public int ExtraDigit { get; set; }
+}
