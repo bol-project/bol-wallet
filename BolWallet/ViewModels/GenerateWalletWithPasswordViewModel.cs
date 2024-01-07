9b65d9f5fae44b83681c3d9753e2789a021b44dc
nmoschos <nmoschos@metamind.gr>
2024-01-07 12:38:27 +0200

    Bump bol.core to version 12 and include corresponding updates

--- BolWallet/ViewModels/GenerateWalletWithPasswordViewModel.cs
+++ BolWallet/ViewModels/GenerateWalletWithPasswordViewModel.cs
@@ -48,7 +48,7 @@ public partial class GenerateWalletWithPasswordViewModel : BaseViewModel
 
             UserData userData = await this._secureRepository.GetAsync<UserData>("userdata");
 
-            var bolWallet = await Task.Run(() => _walletService.CreateWallet(Password, userData.Codename, userData.Edi, privateKey));
+            var bolWallet = await Task.Run(() => _walletService.CreateWalletB(Password, userData.Codename, userData.Edi, privateKey));
 
             userData.BolWallet = bolWallet;
             userData.WalletPassword = Password;
