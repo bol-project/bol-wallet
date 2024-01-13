e21c7b4b5478a2f970e858680ea2eed8d3460b0e
nmoschos <nmoschos@metamind.gr>
2024-01-13 21:33:18 +0200

    Create EncryptedCitizenships page based on user's selection in Step 1

---
+++ BolWallet/Models/EncryptedCitizenshipForm.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BolWallet.Models;
+public class EncryptedCitizenshipForm
+{
+    [Required]
+    public string CountryCode { get; set; }
+    public string CountryName { get; set; }
+    [Required]
+    public string BirthCountryCode { get; set; }
+    [Required]
+    public string Nin { get; set; }
+    [Required]
+    public string SurName { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+    public string ThirdName { get; set; }
+    [Required]
+    public DateTime BirthDate { get; set; }
+}
