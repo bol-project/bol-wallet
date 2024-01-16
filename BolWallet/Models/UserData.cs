759595016cffaeedb300f6b93fe328f8771d2290
nmoschos <nmoschos@metamind.gr>
2024-01-16 19:38:51 +0200

    Save EncryptedCitizenshipForm on submit

--- BolWallet/Models/UserData.cs
+++ BolWallet/Models/UserData.cs
@@ -7,7 +7,8 @@ public class UserData
     public string Codename { get; set; }
     public List<Country> Citizenships { get; set; }
     public string Edi { get; set; }
-    public EncryptedDigitalMatrix EncryptedDigitalMatrix { get; set; }
+    public GenericHashTable GenericHashTable { get; set; }
+    public List<EncryptedCitizenship> CitizenshipMatrices { get; set; }
     public Bol.Core.Model.BolWallet BolWallet { get; set; }
     public string WalletPassword { get; set; }
 }
