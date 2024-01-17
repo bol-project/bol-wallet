8b88a3c4bcf8c35691d8dcada01e02b7874f1c67
nmoschos <nmoschos@metamind.gr>
2024-01-17 18:15:06 +0200

    Implement Capital Letter Validation for EncryptedCitizenshipForm

--- BolWallet/Models/EncryptedCitizenshipForm.cs
+++ BolWallet/Models/EncryptedCitizenshipForm.cs
@@ -9,11 +9,20 @@ public class EncryptedCitizenshipForm
     public string CountryName { get; set; }
     [Required]
     public string Nin { get; set; }
+
     [Required]
+    [RegularExpression("^[A-Z]*$", ErrorMessage = "Only capital letters are allowed.")]
     public string SurName { get; set; }
+
     [Required]
+    [RegularExpression("^[A-Z]*$", ErrorMessage = "Only capital letters are allowed.")]
     public string FirstName { get; set; }
+
+    [RegularExpression("^[A-Z]*$", ErrorMessage = "Only capital letters are allowed.")]
     public string SecondName { get; set; }
+
+    [RegularExpression("^[A-Z]*$", ErrorMessage = "Only capital letters are allowed.")]
     public string ThirdName { get; set; }
+
     public CitizenshipHashTable CitizenshipHashes { get; set; }
 }
