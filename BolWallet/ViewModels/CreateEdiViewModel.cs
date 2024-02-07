5caee2bd5677424e3467c8f01924568908c18749
nmoschos <nmoschos@metamind.gr>
2024-02-07 20:22:35 +0200

    Fix issue with empty citizenships check after back and forth navigation

--- BolWallet/ViewModels/CreateEdiViewModel.cs
+++ BolWallet/ViewModels/CreateEdiViewModel.cs
@@ -172,6 +172,7 @@ public partial class CreateEdiViewModel : BaseViewModel
             var edi = await Task.Run(() => _encryptedDigitalIdentityService.GenerateEDI(edm));
 
             extendedEncryptedDigitalMatrix.Citizenships = edm.Citizenships;
+            extendedEncryptedDigitalMatrix.Citizenships = Array.Empty<string>();
 
             userData.Edi = edi;
             userData.ExtendedEncryptedDigitalMatrix = _encryptedDigitalIdentityService.SerializeMatrix(extendedEncryptedDigitalMatrix);
