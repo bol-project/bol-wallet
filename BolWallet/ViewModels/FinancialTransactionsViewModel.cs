636e69989d177cea711b8cbc0c8abc07451b5615
nmoschos <nmoschos@metamind.gr>
2024-03-21 12:12:57 +0200

    Disable transfer claim and claim functionality for company accounts

--- BolWallet/ViewModels/FinancialTransactionsViewModel.cs
+++ BolWallet/ViewModels/FinancialTransactionsViewModel.cs
@@ -1,25 +1,62 @@
-﻿namespace BolWallet.ViewModels
+﻿using Bol.Core.Abstractions;
+using Bol.Core.Model;
+using CommunityToolkit.Maui.Alerts;
+
+namespace BolWallet.ViewModels
 {
-	public partial class FinancialTransactionsViewModel : BaseViewModel
-	{
-		public FinancialTransactionsViewModel(INavigationService navigationService) : base(navigationService) { }
+    public partial class FinancialTransactionsViewModel : BaseViewModel
+    {
+        private readonly IBolService _bolService;
+        private readonly ISecureRepository _secureRepository;
 
-		[RelayCommand]
-		private async Task NavigateToTransferPage()
-		{
-			await NavigationService.NavigateTo<SendBolViewModel>(true);
-		}
+        public FinancialTransactionsViewModel(
+            INavigationService navigationService,
+            IBolService bolService,
+            ISecureRepository secureRepository)
+            : base(navigationService)
+        {
+            _bolService = bolService;
+            _secureRepository = secureRepository;
+        }
 
-		[RelayCommand]
-		private async Task NavigateToTransferClaimPage()
-		{
-			await NavigationService.NavigateTo<MoveClaimViewModel>(true);
-		}
+        [ObservableProperty]
+        private bool _isCompanyAccount = false;
 
-		[RelayCommand]
-		private async Task Claim()
-		{
-			await NavigationService.NavigateTo<ClaimViewModel>(true);
-		}
-	}
+        [RelayCommand]
+        public async Task Initialize()
+        {
+            try
+            {
+                userData = await _secureRepository.GetAsync<UserData>("userdata");
+
+                if (userData is null) return;
+
+                BolAccount bolAccount = await Task.Run(async () => await _bolService.GetAccount(userData.Codename));
+
+                IsCompanyAccount = bolAccount.CodeName.StartsWith("C");
+            }
+            catch (Exception ex)
+            {
+                await Toast.Make(ex.Message).Show();
+            }
+        }
+
+        [RelayCommand]
+        private async Task NavigateToTransferPage()
+        {
+            await NavigationService.NavigateTo<SendBolViewModel>(true);
+        }
+
+        [RelayCommand]
+        private async Task NavigateToTransferClaimPage()
+        {
+            await NavigationService.NavigateTo<MoveClaimViewModel>(true);
+        }
+
+        [RelayCommand]
+        private async Task Claim()
+        {
+            await NavigationService.NavigateTo<ClaimViewModel>(true);
+        }
+    }
 }
