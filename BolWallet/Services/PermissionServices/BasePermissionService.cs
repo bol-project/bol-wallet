e30dc3643b4a0d028091d717c9e6b1c391e18077
Nikos Chomenidis <nchomenidis@indeavor.com>
2024-03-25 01:13:53 +0200

    Create base class for permission services

---
+++ BolWallet/Services/PermissionServices/BasePermissionService.cs
@@ -0,0 +1,61 @@
+﻿using static Microsoft.Maui.ApplicationModel.Permissions;
+
+namespace BolWallet.Services.PermissionServices;
+
+public abstract class BasePermissionService : IPermissionService
+{
+    protected abstract Task OpenSystemSettingsAsync<T>() where T : BasePermission, new();
+
+    public virtual async Task<PermissionStatus> CheckPermissionAsync<T>() where T : BasePermission, new()
+    {
+        try
+        {
+            PermissionStatus status = await CheckStatusAsync<T>();
+
+            return status;
+        }
+        // From Maui Docs: A PermissionException is thrown if the required permission isn't declared.
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public virtual async Task<PermissionStatus> RequestPermissionAsync<T>() where T : BasePermission, new()
+    {
+        var result = await RequestAsync<T>();
+
+        return result;
+    }
+
+    public virtual async Task PromptToOpenSettingsAsync<T>() where T : BasePermission, new()
+    {
+        var permissionName = typeof(T).Name;
+
+        var accepted = await Application.Current.MainPage.DisplayAlert(
+            "Permission Required",
+            $"To continue, please give {permissionName} permissions for this app in your device settings.",
+            "Open Settings",
+            "Cancel");
+
+        if (accepted)
+        {
+           await OpenSystemSettingsAsync<T>();
+        }
+    }
+
+    public async Task<bool> TryGetPermissionAsync<T>() where T : BasePermission, new()
+    {
+        var initialPermissionStatus = await CheckPermissionAsync<T>();
+
+        if (initialPermissionStatus is PermissionStatus.Granted) return true;
+
+        var result = await RequestPermissionAsync<T>();
+
+        if (result is PermissionStatus.Granted) return true;
+
+        await PromptToOpenSettingsAsync<T>();
+        return false;
+    }
+}
