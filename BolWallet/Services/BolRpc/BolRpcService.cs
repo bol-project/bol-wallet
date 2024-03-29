8e42db25845ae0a9ee001958a790d414e3e9c1f3
Theofanis Tokas <tokastheofanis@gmail.com>
2024-03-29 13:17:26 +0200

    Clean up the service with separate classes for all models and constants

--- BolWallet/Services/BolRpc/BolRpcService.cs
+++ BolWallet/Services/BolRpc/BolRpcService.cs
@@ -5,50 +5,16 @@ using SimpleResults;
 
 namespace BolWallet.Services.BolRpc;
 
-internal readonly record struct BolRpcRequest(
-    string Method,
-    string[] Params,
-    string JsonRpcVersion = "2.0",
-    int Id = 1);
-
-internal readonly record struct BolRpcErrorResult(int Code, string Message);
-
-internal readonly record struct BolRpcResponse<T>(T Result, BolRpcErrorResult? Error);
-
-internal static class BolRpcResponseExtensions
-{
-    internal static Result<T> ToResult<T>(this BolRpcResponse<T> response) => response switch
-    {
-        { Error: not null } => Result.NotFound([
-            $"BoL RPC Error Code: {response.Error.Value.Code}",
-            $"BoL RPC Error Message: {response.Error.Value.Message}"
-        ]),
-        { Result: null } => Result.CriticalError("No result found"),
-        { Result: not null } => Result.ObtainedResource(response.Result)
-    };
-}
-
 internal class BolRpcService(HttpClient client, ILogger<BolRpcService> logger) : IBolRpcService
 {
-    private static JsonSerializerOptions JsonSerializerDefaults => new()
-    {
-        PropertyNameCaseInsensitive = true,
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-    };
-    
-    private static readonly BolRpcRequest s_getBolContractHashRequest = new("getbolhash", []);
-    private static BolRpcRequest s_getAccountRequest(string codename) => new("getAccount", [codename]);
-    
     public async Task<Result<string>> GetBolContractHash(CancellationToken token = default) =>
-        await PerformRpcRequest<string>(s_getBolContractHashRequest, token: token);
+        await PerformRpcRequest<string>(BolRpcMethods.GetBolContractHashRequest, token: token);
 
     public async Task<Result<BolAccount>> GetBolAccount(string codename, CancellationToken token = default) =>
-        await PerformRpcRequest<BolAccount>(s_getAccountRequest(codename), token: token);
+        await PerformRpcRequest<BolAccount>(BolRpcMethods.GetAccountRequest(codename), token: token);
 
     private async Task<Result<T>> PerformRpcRequest<T>(
         BolRpcRequest request,
-        string resultKey = "result",
-        string errorKey = "error",
         CancellationToken token = default)
     {
         try
@@ -56,7 +22,7 @@ internal class BolRpcService(HttpClient client, ILogger<BolRpcService> logger) :
             using var response = await client.PostAsJsonAsync(
                 null as string,
                 request,
-                JsonSerializerDefaults,
+                BolRpcConstants.JsonSerializerDefaults,
                 token);
 
             if (!response.IsSuccessStatusCode)
---
+++ BolWallet/Services/BolRpc/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using SimpleResults;
+
+namespace BolWallet.Services.BolRpc;
+
+internal static class Extensions
+{
+    internal static Result<T> ToResult<T>(this BolRpcResponse<T> response) => response switch
+    {
+        { Error: not null } => Result.NotFound([
+            $"BoL RPC Error Code: {response.Error.Value.Code}",
+            $"BoL RPC Error Message: {response.Error.Value.Message}"
+        ]),
+        { Result: null } => Result.CriticalError("No result found"),
+        { Result: not null } => Result.ObtainedResource(response.Result)
+    };
+}
