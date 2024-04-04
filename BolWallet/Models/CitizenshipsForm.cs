99464b0bd981e6f984f9b273df38a4ccec05bc04
Nikos Chomenidis <nchomenidis@indeavor.com>
2024-04-05 00:17:18 +0300

    Make form an Observable object

--- BolWallet/Models/CitizenshipsForm.cs
+++ BolWallet/Models/CitizenshipsForm.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace BolWallet.Models;
-public class CitizenshipsForm
+
+public class CitizenshipsForm : ObservableObject
 {
     [Required]
     public string FirstCountry { get; set; }
     public string SecondCountry { get; set; }
     public string ThirdCountry { get; set; }
 }
+
