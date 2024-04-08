9470e1505d68a37bf310173694b7efb17212ff8d
Nikos Chomenidis <nchomenidis@indeavor.com>
2024-04-08 17:40:14 +0300

    Add more operations at MediaService and refactor previous implementations

--- BolWallet/Services/Abstractions/IMediaService.cs
+++ BolWallet/Services/Abstractions/IMediaService.cs
@@ -4,17 +4,41 @@ namespace BolWallet.Services.Abstractions;
 public interface IMediaService
 {
     /// <summary>
+    /// Pick a file from the device.
+    /// </summary>
+    /// <returns></returns>
+    Task<FileResult> PickFileAsync();
+    
+    /// <summary>
+    /// Pick an audio file from the device.
+    /// </summary>
+    /// <returns></returns>
+    Task<FileResult> PickAudioFileAsync();
+    
+    /// <summary>
     /// Takes a photo and saves it to the provided location.
     /// Calling this method will request the necessary permissions.
     /// </summary>
     /// <param name="saveLocation"></param>
     /// <returns></returns>
-    Task<string> TakePhotoAsync(string saveLocation);
+    Task<FileResult> TakePhotoAsync(string saveLocation);
     
     /// <summary>
     /// Pick a photo from the device.
     /// No permissions are requested when calling this method.
     /// </summary>
     /// <returns></returns>
-    Task<FileResult?> PickPhotoAsync();
+    Task<FileResult> PickPhotoAsync();
+    
+    /// <summary>
+    /// Starts recording audio and returns a boolean indicating whether the recording was started successfully.
+    /// </summary>
+    /// <returns></returns>
+    Task<bool> StartRecordingAudioAsync();
+    
+    /// <summary>
+    /// Stops recording audio and returns the recorded audio file.
+    /// </summary>
+    /// <returns></returns>
+    Task<FileResult> StopRecordingAudioAsync();
 }
--- BolWallet/Services/MediaService.cs
+++ BolWallet/Services/MediaService.cs
@@ -1,43 +1,148 @@
 ﻿#nullable enable
+using Microsoft.Extensions.Logging;
+using Plugin.Maui.Audio;
+
 namespace BolWallet.Services;
 
 public class MediaService : IMediaService
 {
     private readonly IMediaPicker _mediaPicker;
+    private readonly IFilePicker _filePicker;
+    private readonly IAudioRecorder _audioRecorder;
     private readonly IPermissionService _permissionService;
+    private readonly ILogger<MediaService> _logger;
 
-    public MediaService(IMediaPicker mediaPicker, IPermissionService permissionService)
+    private readonly FilePickerFileType _supportedFileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+    {
+        { DevicePlatform.iOS, new[] { "com.adobe.pdf", "public.image", "public.audio" } },
+        { DevicePlatform.Android, new[] { "application/pdf", "image/*", "audio/*" } },
+        { DevicePlatform.MacCatalyst, new[] { "pdf", "png", "jpeg", "jpg", "public.image", "public.audio" } },
+        { DevicePlatform.WinUI, new[] { ".pdf", ".gif", ".mp3", ".png", ".jpg", ".jpeg" } },
+    });
+    
+    private readonly FilePickerFileType _supportedAudioFileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+    {
+        { DevicePlatform.iOS, new[] {"public.audio" , ".mp3"} },
+        { DevicePlatform.Android, new[] {  "audio/*" } },
+        { DevicePlatform.MacCatalyst, new[] {"public.audio", ".mp3" } },
+        { DevicePlatform.WinUI, new[] { ".mp3" } }
+    });
+
+    public MediaService(
+        IMediaPicker mediaPicker,
+        IFilePicker filePicker,
+        IAudioManager audioManager,
+        IPermissionService permissionService,
+        ILogger<MediaService> logger)
     {
         _mediaPicker = mediaPicker;
+        _filePicker = filePicker;
+        _audioRecorder = audioManager.CreateRecorder();
         _permissionService = permissionService;
+        _logger = logger;
     }
 
-    public async Task<string> TakePhotoAsync(string saveLocation)
+    public async Task<FileResult> PickFileAsync()
+    {
+        return await PickFileAsync(_supportedFileTypes, "Pick a file");
+    }
+
+    public async Task<FileResult> PickAudioFileAsync()
+    {
+        return await PickFileAsync(_supportedAudioFileTypes, "Pick an audio file");
+    }
+
+    public async Task<FileResult> TakePhotoAsync(string saveLocation)
     {
         var hasGivenPermission = await _permissionService.TryGetPermissionAsync<Permissions.Camera>();
 
-        if (!hasGivenPermission) return String.Empty;
+        if (!hasGivenPermission) throw new PermissionException("The user has not given permission to access the camera.");
 
         var photo = await _mediaPicker.CapturePhotoAsync();
 
-        if (photo is null) return String.Empty;
+        if (photo is null) throw new InvalidOperationException("The photo capture was not successful.");
 
         // save the file into the provided save location
-        var localFilePath = Path.Combine(saveLocation, photo.FileName);
+        var photoFilePath = Path.Combine(saveLocation, photo.FileName);
 
         await using var sourceStream = await photo.OpenReadAsync();
 
-        await using var fileStream = new FileStream(localFilePath, FileMode.Create, FileAccess.Write);
+        await using var fileStream = new FileStream(photoFilePath, FileMode.Create, FileAccess.Write);
     
         await sourceStream.CopyToAsync(fileStream);
 
-        return localFilePath;
+        return new FileResult(photoFilePath);
     }
 
-    public async Task<FileResult?> PickPhotoAsync()
+    public async Task<FileResult> PickPhotoAsync()
     {
-        var photo = await _mediaPicker.PickPhotoAsync();
+        var photo = await _mediaPicker.PickPhotoAsync(new MediaPickerOptions
+        {
+            Title = "Pick a photo"
+        });
 
-        return photo;
+        if (photo is not null) return photo;
+
+        var exception = new FileNotFoundException("The requested photo was not found.");
+        _logger.LogError(exception, "No photo was picked.");
+        throw exception;
+    }
+
+    public async Task<bool> StartRecordingAudioAsync()
+    {
+        var hasGivenPermission = await _permissionService.TryGetPermissionAsync<Permissions.Microphone>();
+
+        if (!hasGivenPermission)
+        {
+            var exception = new PermissionException("The user has not given permission to access the microphone.");
+            _logger.LogError(exception, "Cannot record audio without microphone permission.");
+            throw exception;
+        }
+        
+        if (_audioRecorder.IsRecording)
+        {
+            return false;
+        }
+
+        await _audioRecorder.StartAsync();
+        return true;
+    }
+
+    public async Task<FileResult> StopRecordingAudioAsync()
+    {
+        if (!_audioRecorder.IsRecording)
+        {
+            var exception = new InvalidOperationException("No audio is being recorded.");
+            _logger.LogError(exception, "Cannot stop recording audio without starting it first.");
+            throw exception;
+        }
+
+        var recording = await _audioRecorder.StopAsync();
+
+        var audioStream = recording.GetAudioStream();
+
+        string directoryPath = FileSystem.AppDataDirectory; // or FileSystem.CacheDirectory for temporary files
+        string filePath = Path.Combine(directoryPath, "personalVoice.mp3");
+
+        await using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+        {
+            await audioStream.CopyToAsync(fileStream);
+        }
+
+        return new FileResult(filePath);
+    }
+
+    private async Task<FileResult> PickFileAsync(FilePickerFileType supportedFileTypes, string pickerTitle)
+    {
+        var fileResult = await _filePicker.PickAsync(new PickOptions
+        {
+            FileTypes = supportedFileTypes, PickerTitle = pickerTitle
+        });
+
+        if (fileResult is not null) return fileResult;
+
+        var exception = new FileNotFoundException("The requested file was not found.");
+        _logger.LogError(exception, $"No file was picked with picker title: {pickerTitle}");
+        throw exception;
     }
 }
