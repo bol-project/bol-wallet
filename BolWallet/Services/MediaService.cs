abd554dafabca48537be8e701dc1708035870780
Nikos Chomenidis <nchomenidis@indeavor.com>
2024-04-13 13:24:55 +0300

    Support more audio files for Windows platform

--- BolWallet/Services/MediaService.cs
+++ BolWallet/Services/MediaService.cs
@@ -25,7 +25,7 @@ public class MediaService : IMediaService
         { DevicePlatform.iOS, new[] {"public.audio" } },
         { DevicePlatform.Android, new[] {  "audio/*" } },
         { DevicePlatform.MacCatalyst, new[] {"public.audio", ".mp3" } },
-        { DevicePlatform.WinUI, new[] { ".mp3" } }
+        { DevicePlatform.WinUI, new[] { ".mp3", ".wav", ".wma", ".m4a", ".flac" } }
     });
 
     public MediaService(
