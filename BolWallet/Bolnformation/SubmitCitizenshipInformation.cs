49dfc9e10f8434ca1232c0ea7a1253495344835b
Nikos Chomenidis <nchomenidis@indeavor.com>
2024-04-16 18:30:49 +0300

    Create new folder for bol information and add class for citizenships information

---
+++ BolWallet/Bolnformation/SubmitCitizenshipInformation.cs
@@ -0,0 +1,13 @@
+﻿namespace BolWallet.Bolnformation;
+
+public static class SubmitCitizenshipInformation
+{
+    public const string Title = "Submit Citizenship Information";
+    public const string Description =
+        "We will not use your files for any other purpose than verifying your citizenship" +
+        " and we will not share your files with any third party. " + "" +
+        "All your files will be encrypted and stored in a secure way.";
+    public const string Content =
+        "Citizens who have more than one citizenship will register only once in the community" +
+        " like other citizens and will choose one of the citizenships for their registration.";
+}
