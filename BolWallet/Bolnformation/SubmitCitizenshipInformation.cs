34ad3f5026576d01665850421c11750d54c1213c
Nikos Chomenidis <nchomenidis@indeavor.com>
2024-04-18 13:53:42 +0300

    Update more info message

--- BolWallet/Bolnformation/SubmitCitizenshipInformation.cs
+++ BolWallet/Bolnformation/SubmitCitizenshipInformation.cs
@@ -4,9 +4,10 @@ public static class SubmitCitizenshipInformation
 {
     public const string Title = "Submit Citizenship Information";
     public const string Description =
-        "We will not use your files for any other purpose than verifying your citizenship" +
-        " and we will not share your files with any third party. " + "" +
-        "All your files will be encrypted and stored in a secure way.";
+        "Your files will remain exclusively on your device and will not be transferred " +
+        "to any external servers or third parties. The only instance where your files will be shared " +
+        "is directly with the certifying authority during the verification process. " +
+        "No copies of your files will be made or stored elsewhere.";
     public const string Content =
         "Citizens who have more than one citizenship will register only once in the community" +
         " like other citizens and will choose one of the citizenships for their registration.";
