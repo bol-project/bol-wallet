2de1d9062835cf3a740af9bea23024a95ce31251
nmoschos <nmoschos@metamind.gr>
2024-04-25 16:14:15 +0300

    Create page to view and save certification documents

---
+++ BolWallet/Views/DownloadCertificationDocumentsPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BolWallet.Views;
+
+public partial class DownloadCertificationDocumentsPage : ContentPage
+{
+    public DownloadCertificationDocumentsPage()
+    {
+        InitializeComponent();
+    }
+}
