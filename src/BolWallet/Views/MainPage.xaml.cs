8cbe80201ac28d41db4091cfdc64a982697a45d2
Theofanis Tokas <tokastheofanis@gmail.com>
2024-10-02 15:59:01 +0300

    Remove initializing logic since it's not needed here anymore

--- src/BolWallet/Views/MainPage.xaml.cs
+++ src/BolWallet/Views/MainPage.xaml.cs
@@ -1,49 +1,10 @@
-﻿using Bol.Core.Model;
-using CommunityToolkit.Maui.Alerts;
-using Microsoft.Extensions.Options;
-
-namespace BolWallet;
+﻿namespace BolWallet;
 
 public partial class MainPage : ContentPage
 {
-    private readonly IBolRpcService _bolRpcClient;
-    private readonly IOptions<BolConfig> _bolConfigOptions;
-    RadialControlViewModel RadialVM = new();
-
-    public MainPage(
-        MainViewModel mainViewModel,
-        IBolRpcService bolRpcClient,
-        IOptions<BolConfig> bolConfigOptions)
+    public MainPage(MainViewModel mainViewModel)
     {
-		InitializeComponent();
+        InitializeComponent();
 		BindingContext = mainViewModel;
-        _bolRpcClient = bolRpcClient;
-        _bolConfigOptions = bolConfigOptions;
-    }
-
-    protected override async void OnAppearing()
-    {
-        base.OnAppearing();
-
-        await Dispatcher.DispatchAsync(TrySetBolContractHash);
-    }
-
-    private async Task TrySetBolContractHash()
-    {
-        if (!string.IsNullOrWhiteSpace(_bolConfigOptions.Value.Contract))
-        {
-            return;
-        }
-
-        var result = await _bolRpcClient.GetBolContractHash();
-        if (result.IsFailed)
-        {
-            await Toast.Make(result.Message).Show();
-            return;
-        }
-
-        _bolConfigOptions.Value.Contract = result.Data;
-        return;
     }
 }
-
