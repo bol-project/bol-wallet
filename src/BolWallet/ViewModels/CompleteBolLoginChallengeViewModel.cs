3f7ffa36a5ada855ac039f2d7356acf4b9d19f2d
Theofanis Tokas <tokastheofanis@gmail.com>
2024-10-20 23:25:55 +0300

    Add logging when completing a BoL Challenge

--- src/BolWallet/ViewModels/CompleteBolLoginChallengeViewModel.cs
+++ src/BolWallet/ViewModels/CompleteBolLoginChallengeViewModel.cs
@@ -2,6 +2,7 @@
 using Bol.Cryptography;
 using Bol.Cryptography.Abstractions;
 using CommunityToolkit.Maui.Alerts;
+using Microsoft.Extensions.Logging;
 
 namespace BolWallet.ViewModels;
 
@@ -14,41 +15,54 @@ public partial class CompleteBolLoginChallengeViewModel : BaseViewModel
     private readonly IECCurveSigner _signer;
     private readonly IBase64Encoder _base64Encoder;
     private readonly IBolChallengeService _bolChallengeService;
+    private readonly ILogger<CompleteBolLoginChallengeViewModel> _logger;
 
     public CompleteBolLoginChallengeViewModel(
         INavigationService navigationService,
         IContextAccessor contextAccessor,
         IECCurveSigner signer,
         IBase64Encoder base64Encoder,
-        IBolChallengeService bolChallengeService) : base(navigationService)
+        IBolChallengeService bolChallengeService,
+        ILogger<CompleteBolLoginChallengeViewModel> logger) : base(navigationService)
     {
         _contextAccessor = contextAccessor;
         _signer = signer;
         _base64Encoder = base64Encoder;
         _bolChallengeService = bolChallengeService;
+        _logger = logger;
     }
 
     [RelayCommand]
     private async Task CompleteBolChallenge(CancellationToken token = default)
     {
-        var context = _contextAccessor.GetContext();
-        var keyPair = context.SocialAddress.Value;
-        var publicKeyBytes = keyPair.PublicKey.ToBytes();
-
-        var challenge = _base64Encoder.Decode(Challenge);
-        var signature = _signer.Sign(challenge, keyPair.PrivateKey, publicKeyBytes);
-        
-        var signatureString = _base64Encoder.Encode(signature);
-        var publicKey = _base64Encoder.Encode(publicKeyBytes);
-        
-        var result = await _bolChallengeService.CompleteChallenge(Challenge, signatureString, publicKey, context.CodeName, token);
-        if (result.IsFailed)
+        try
         {
-            await Toast.Make(result.Message).Show(token);
-            return;
-        }
+            var context = _contextAccessor.GetContext();
+            var keyPair = context.SocialAddress.Value;
+            var publicKeyBytes = keyPair.PublicKey.ToBytes();
 
-        await Toast.Make("BoL Login Challenge completed!").Show(token);
-        await NavigationService.NavigateTo<MainWithAccountViewModel>(true);
+            var challenge = _base64Encoder.Decode(Challenge);
+            var signature = _signer.Sign(challenge, keyPair.PrivateKey, publicKeyBytes);
+
+            var signatureString = _base64Encoder.Encode(signature);
+            var publicKey = _base64Encoder.Encode(publicKeyBytes);
+
+            var result =
+                await _bolChallengeService.CompleteChallenge(Challenge, signatureString, publicKey, context.CodeName,
+                    token);
+            if (result.IsFailed)
+            {
+                await Toast.Make(result.Message).Show(token);
+                return;
+            }
+
+            await Toast.Make("BoL Login Challenge completed!").Show(token);
+            await NavigationService.NavigateBack();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error completing BoL Challenge {BoLChallenge}", Challenge);
+            await Toast.Make($"BoL Login Challenge failed with error:{Environment.NewLine}{ex.Message}").Show(token);
+        }
     }
 }
