facd6eda77974dcb8a189f9c955f4c2bee0925eb
Theofanis Tokas <tokastheofanis@gmail.com>
2024-10-23 00:06:35 +0300

    Add build script for android

---
+++ src/scripts/android/publish-android-ad-hoc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dotnet publish -f net8.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=$1 -p:AndroidSigningKeyAlias=bolwallet -p:AndroidSigningKeyPass=file:$2 -p:AndroidSigningStorePass=file:$2 ./../../BolWallet/BolWallet.csproj
