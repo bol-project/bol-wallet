4a60160302c5e11d66dcc49c2f11eb0fcf453ddf
Theofanis Tokas <tokastheofanis@gmail.com>
2024-11-13 02:36:01 +0200

    Update build scripts to target net9

--- src/scripts/android/publish-android-ad-hoc.sh
+++ src/scripts/android/publish-android-ad-hoc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dotnet publish -f net8.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=$1 -p:AndroidSigningKeyAlias=bolwallet -p:AndroidSigningKeyPass=file:$2 -p:AndroidSigningStorePass=file:$2 ./../../BolWallet/BolWallet.csproj
+dotnet publish -f net9.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=$1 -p:AndroidSigningKeyAlias=bolwallet -p:AndroidSigningKeyPass=file:$2 -p:AndroidSigningStorePass=file:$2 ./../../BolWallet/BolWallet.csproj
